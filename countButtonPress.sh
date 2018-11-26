b2134a7ff398d21bbd9210a34021d50abaa6a44a
chaohan715 <chaohan715@gmail.com>
2018-11-26 10:12:41 -0500

    Update countButtonPress.sh

--- countButtonPress.sh
+++ countButtonPress.sh
@@ -1,21 +1,16 @@
-#!/bin/bash
-i=0
-while test $i -eq 15; do
-  j=0
-  while test $j -eq 0; do
-    k=$(gpio read 5)
-    if test "$k" -eq 1
-    then
-      echo "Wait For Button Press"
-    else
-      echo "Button Pressed"
-      j=1
-    fi
-  done
-  let "i=$i+1"
-  ./setbits.sh $i
-  sleep 0.25
+!/bin/bash
+i=0 
+while test $i -le 15; do
+  j=$(gpio read 5)
+  ./waitForButtonPress.sh
+  if test "$j" -eq 0
+  then
+    let "i=$i+1"
+    ./setbits.sh $i
+    sleep 0.25
+  fi
 done
 gpio write 4 1
 sleep 1
 ./initMode.sh
+
