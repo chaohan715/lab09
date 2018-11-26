1ab916ceb73ed36b6d386437a3fde92287181493
chaohan715 <chaohan715@gmail.com>
2018-11-26 00:10:21 -0500

    Update and rename wairForButtonPress.sh to waitForButtonPress.sh

--- wairForButtonPress.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-z=$(gpio read 5)
-until test $z -eq 0; do
-  echo "Wait For Button Press"
-done
---
+++ waitForButtonPress.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while true; do
+  i=$(gpio read 5)
+  if test "$i" -eq 1
+  then
+    echo "Wait For Button Press"
+    sleep 1
+  else
+    echo "Button is Pressed"
+    sleep 1
+done
