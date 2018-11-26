80f6e12d8cc5cccaf86cd21d84c270597a1a4e9e
chaohan715 <chaohan715@gmail.com>
2018-11-26 10:08:11 -0500

    Update waitForButtonPress.sh

--- waitForButtonPress.sh
+++ waitForButtonPress.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-while true; do
+i=$(gpio read 5)
+while test $i -eq 1; do
   i=$(gpio read 5)
-  if test "$i" -eq 1
-  then
-    echo "Wait For Button Press"
-    sleep 1
-  else
-    echo "Button is Pressed"
-    sleep 1
-  fi
+  echo "Wait For Button Press"
+  sleep 0.25
+  while test $i -eq 0; do
+    i=$(gpio read 5)
+    echo "Button Pressed"
+    sleep 0.25
+  done
 done
