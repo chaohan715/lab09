444d8380a8927ee2dc267d73ea22c03f77e37fee
chaohan715 <chaohan715@gmail.com>
2018-11-26 01:54:43 -0500

    Update initMode.sh

--- initMode.sh
+++ initMode.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 z=0
-for i in 0 1 2 3 4 ; do
-  gpio mode $z out
-  gpio write $z 0
+for i in 0 1 2 3 4 5; do
+  if test "$z" -eq 5
+  then
+    gpio mode $z in
+    gpio mode $z up
+  else
+    gpio mode $z out
+    gpio write $z 0
+  fi
+  let "z=$z+1"
 done
-
-gpio mode 5 in
-gpio mode 5 up
