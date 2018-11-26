84c00fd5218b9680e7c1bba395ad0a61afadfe0b
chaohan715 <chaohan715@gmail.com>
2018-11-26 01:53:46 -0500

    Create countButtonPress.sh

---
+++ countButtonPress.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+i=0
+while test $i -eq 15; do
+  j=0
+  while test $j -eq 0; do
+    k=$(gpio read 5)
+    if test "$k" -eq 1
+    then
+      echo "Wait For Button Press"
+    else
+      echo "Button Pressed"
+      j=1
+    fi
+  done
+  let "i=$i+1"
+  ./setbits.sh $i
+  sleep 0.25
+done
+./initMode.sh
